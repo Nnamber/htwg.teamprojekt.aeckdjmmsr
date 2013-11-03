488b7243e49e7633d4c1f5d8f82fac9c06751dcc
unknown <micmeist@.(none)>
2013-11-03 18:14:34 +0100

    Added collapsing Klicking on Thema-checkboxes now check/uncheck all checkboxes of its questions

---
+++ Admin-Seite/js/omm_display.js
@@ -0,0 +1,26 @@
+function display() {
+	//private variables
+	var themaTable;
+
+	//pulbic
+	this.init = function(table) {
+		themaTable = table;
+
+		initCheckThemaEventHandler();
+	};
+
+	//private
+	function initCheckThemaEventHandler() {
+		var themaCheckboxes = jQuery(themaTable).find(omm_cssSelector_themaRow + " :checkbox");
+		jQuery(themaCheckboxes).change(function(eventObject) {
+			var checkboxes = jQuery(this).parents(omm_cssSelector_themaRow).find(":checkbox");
+			
+			if (jQuery(this).prop('checked') == true) {
+				checkboxes.prop("checked", true);
+			}else{
+				checkboxes.prop("checked", false);
+			}
+		});
+	}
+
+};
---
+++ Admin-Seite/js/omm_main.js
@@ -0,0 +1,12 @@
+var omm_display = new display();
+var omm_cssSelector_themaRow = ".omm_thema-row";
+
+jQuery(document).ready(function(){
+	
+	var thematable = jQuery("#omm_thema-table");
+
+	omm_display.init(thematable);
+	
+	
+	
+});
