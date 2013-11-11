a89c5f2fb8383ac3ad5b23298ca9b6a9048a2521
unknown <Armin@.(none)>
2013-11-11 15:21:27 +0100

    Fragen verarbeiten und anzeigen

--- Admin-Seite/js/xmlReader.js
+++ Admin-Seite/js/xmlReader.js
@@ -5,9 +5,37 @@ $(document).ready(function() {
 	function parse(document) {
 		var topicCounter = 1;
 		$(document).find("course").find("lesson").each(function() {
-			$("#omm_thema-table").append('<div class="panel panel-default omm_thema-row"><div class="panel-heading">' + '<div class="panel-title"><input type="checkbox"/><a data-toggle="collapse" data-parent="#omm_thema-table" href="#collapse'+topicCounter+'">' + ' Thema ' + topicCounter + ': <span class="omm_lesson-title">' + $(this).attr("name") + '</span></a></div></div></div>');
+			$("#omm_thema-table").append(htmlLesson(topicCounter, $(this).attr("name")) + questionDivHtml($(this), topicCounter)+ '</div>');
 			topicCounter++;
 		});
+	}
+
+	function questionDivHtml(xmlLessonObject, topicCounter) {
+		var x = '<div id="collapse' + topicCounter + '" class="panel-collapse collapse">';
+		x += '<div class="omm_panel-body">';
+		x += '<table class="table table-striped table-hover">';
+		var questionCounter = 1;
+
+		$(xmlLessonObject).find("question").each(function() {
+			// alert($(this).attr("name"));
+
+			x += htmlQuestion(questionCounter, $(this).attr("name"));
+			questionCounter++;
+		});
+
+		x += '</table></div></div>';
+		return x;
+	}
+
+	function htmlLesson(lessonCounter, lessonTitle) {
+
+		var lessonHtml = '<div class="panel panel-default omm_thema-row"><div class="panel-heading"><div class="panel-title"><input type="checkbox"/><a data-toggle="collapse" data-parent="#omm_thema-table" href="#collapse' + lessonCounter + '"> Thema ' + lessonCounter + ': <span class="omm_lesson-title">' + lessonTitle + '</span></a></div></div>';
+		return lessonHtml;
+	}
+
+	function htmlQuestion(questionCounter, questionTitle) {
+		var questionHtml = '<tr ><td><input type="checkbox" class="pull-right"/></td><td>Frage ' + questionCounter + ': <span class="omm_question-title">' + questionTitle + '</span></td></tr>';
+		return questionHtml;
 	}
 
 	function readXml() {
