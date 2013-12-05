3244b03111d22337ec9c534ab487def7d27d2ecc
Agraphie <real_freeren@yahoo.de>
2013-12-05 10:04:09 +0100

    Optimised drag and drop. Optimised styling. Drag and drop answers now always swap places. Increased dropable area for answers. Added some styling.

--- HTML-Seite/js/omm_dragAndDrop.js
+++ HTML-Seite/js/omm_dragAndDrop.js
@@ -3,30 +3,51 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+    //set the id of the parent of the event who triggered the "drag" event
     ev.dataTransfer.setData("parentId", $(ev.target).parent().attr('id'));
+        
+    //set the id of the element who triggered the event
     ev.dataTransfer.setData("dragId", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
 
-    //ziel darf nicht die Klasse "omm_draggable" haben, also kein drag und drop objekt (Antwort) sein
-    //ziel darf keine Kinder haben oder muss das große Antwortfeld sein
+    //get the id of the element who triggered the event
     var data = ev.dataTransfer.getData("dragId");
-    if (!$(ev.target).hasClass("omm_draggable") && ($(ev.target).children().length == 0 || $(ev.target).attr('id') == 'answerField')) {
+    
+    //Dropable target is not a draggable object, is not a dropable targt with an answer already or is the big answer field
+    if (!$(ev.target).hasClass("omm_draggable") && ($(ev.target).children().length === 0 || $(ev.target).hasClass('omm_answer-field-big'))) {
         ev.target.appendChild(document.getElementById(data));
-    } else {
+        
+        //Dropable target is either an answer or an answer field with an answer already
+    } else {  
+        //Dropable target is a dragable answer
         if ($(ev.target).hasClass("omm_draggable")) {
-            //get parent und caste es zu DOM objekt
+            //get the answer field in which the dragable answer is located
             var parent = $(ev.target).parent().get(0);
+            //get parent und caste es zu DOM objekt
             var dragParent = ev.dataTransfer.getData("parentId");
-            //dragParent = $(dragParent).get(0);
-            console.log(dragParent);
-            //dragParent.appendChild(document.getElementById(ev.target));
+            
+            //append the element who triggered the event to its target
             parent.appendChild(document.getElementById(data));
+            
+            //append the element located in the answer field to the parent of the element who triggered the event
+            $("#" + dragParent).append(ev.target);
+            
+        //Dropable target is a dropable answer field
+        } else if (!$(ev.target).hasClass("omm_draggable") &&  $(ev.target).children().length === 1){
+            //get child of the answer field and cast to DOM object
+            var child = $(ev.target).children().first().get(0);
+            //get parent of the element who triggered the event
+            var dragParent = ev.dataTransfer.getData("parentId");
+                        console.log(dragParent);
 
-         $("#"+dragParent).append(ev.target);
-            //$("#answerField").append(ev.target);
+            //append the element who triggered the event to its target
+            ev.target.appendChild(document.getElementById(data));
+            
+            //append the element located in the answer field to the parent of the element who triggered the event
+            $("#" + dragParent).append(child);
         }
     }
 
